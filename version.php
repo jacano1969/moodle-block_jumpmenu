fb64b8451615e360954d6c3a4f4c635a8be12db8
Daniel Somers <d.somers@ulcc.ac.uk>
2013-02-26 13:20:40 +0000

    updated version

--- version.php
+++ version.php
@@ -6,6 +6,6 @@
  * Time: 11:53 AM
  * To change this template use File | Settings | File Templates.
  */
-$plugin->version = 2012010500; // YYYYMMDDHH (year, month, day, 24-hr time)
-$plugin->requires = 2010112400; // YYYYMMDDHH (This is the release version for Moodle 2.0)
+$plugin->version = 2012061700; // YYYYMMDDHH (year, month, day, 24-hr time)
+$plugin->requires = 2012061700; // YYYYMMDDHH (This is the release version for Moodle 2.0)
 ?>
